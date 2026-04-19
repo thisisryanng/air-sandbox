3d609a0778acf5c59cff51d148bbb3b750c3b5f8
Ryan <this.is.ryanng@gmail.com>
2026-04-19 16:22:40 +0200

    feat(T-SANDBOX-001): Create project structure and main utility module

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""
+Sandbox Project - Main Package
+
+This package provides utility functions and modules for sandbox testing.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Project AIR"
+
+# Import main utilities for easy access
+from .utils import *
+
+__all__ = [
+    "get_version",
+    "hello_world",
+]
